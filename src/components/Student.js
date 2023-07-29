30c34cba650a4d80cd0b540fb3ac296a268e5883
asnganje <abd.nganje@gmail.com>
2023-07-29 17:43:40 +0300

    update package json for deployment

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,22 +1,14 @@
 import React from "react";
 
-const Student = ({response}) => {
+const Student = () => {
 
-    if(!response) {
-        return (
-            <div>
-                <h3>Sorry, no student data captured yet!</h3>
-            </div>
-        )
-    }
+   
+
     return(
         <div>
             <h1>Student's details</h1>
             <div>
-                <p>firstName: {response.firstName}</p>
-                <p>Last Name: {response.lastName}</p>
-                <p>Contact Email: {response.email}</p>
-                <p>Gender: {response.gender}</p>
+
             </div>
         </div>
     )
